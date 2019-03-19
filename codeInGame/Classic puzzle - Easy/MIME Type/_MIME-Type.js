6965d352e6f41d7906f386e657bd4df85c389d60
alan1tavares <alan1tavares@gmail.com>
2019-03-19 17:06:39 -0300

    Implementado a 1a verssão do _MIME-Type.js

--- codeInGame/Classic puzzle - Easy/MIME Type/_MIME-Type.js
+++ codeInGame/Classic puzzle - Easy/MIME Type/_MIME-Type.js
@@ -2,35 +2,45 @@
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+const { EOL } = require('os');
 
-const N = parseInt(readline()); // Number of elements which make up the association table.
-const Q = parseInt(readline()); // Number Q of file names to be analyzed.
-const obj = {};
-
-for (let i = 0; i < N; i++) {
-    var inputs = readline().split(' ');
-    const EXT = inputs[0].toLowerCase(); // file extension
-    const MT = inputs[1]; // MIME type.
-
-    obj[EXT] = MT;
-}
-for (let i = 0; i < Q; i++) {
-    const FNAME = readline().toLowerCase(); // One file name per line.
-
-    let match = FNAME.match(/\.[a-z]+/g);
-    match = match ? match[match.length - 1]
-        .toLowerCase()
-        .replace('.', '') : '';
-
-    console.error('-match:', match);
-    console.error('-FNAME:', FNAME);
-
-    const EXT = match;
-    if (obj.hasOwnProperty(EXT)) {
-        console.log(obj[EXT]);
-    } else console.log('UNKNOWN');
+let input = '';
+let lines = [];
+const readline = function () {
+    return lines.shift();
 }
 
+process.stdin.on('data', c => input += c);
+process.stdin.on('end', () => {
+    lines = input.split(EOL);
+
+    const N = parseInt(readline()); // Number of elements which make up the association table.
+    const Q = parseInt(readline()); // Number Q of file names to be analyzed.
+    const obj = {};
+
+    for (let i = 0; i < N; i++) {
+        var inputs = readline().split(' ');
+        const EXT = inputs[0].toLowerCase(); // file extension
+        const MT = inputs[1]; // MIME type.
+
+        obj[EXT] = MT;
+    }
+    for (let i = 0; i < Q; i++) {
+        const FNAME = readline().toLowerCase(); // One file name per line.
+
+        // let match = FNAME.match(/\.([a-z]|[0-9])+/g);
+        let match = FNAME.split('.');
+        match = match.length > 1 ? match[match.length - 1]
+            .toLowerCase() : '';
+            // .slice(1) : '';
+
+        const EXT = match;
+        if (obj.hasOwnProperty(EXT)) {
+            console.log(obj[EXT]);
+        } else console.log('UNKNOWN');
+    }
+
+});
 // Write an action using console.log()
 // To debug: console.error('Debug messages...');
 
