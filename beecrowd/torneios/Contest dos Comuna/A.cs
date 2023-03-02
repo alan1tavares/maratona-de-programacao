89460b3b67cf34f6dd8e1b81d0f1bbef69ef590f
Alan Tavares <alan1tavares@gmail.com>
2023-03-01 23:24:52 -0300

    resolução do Problema A do Torneio do Comunas

---
+++ beecrowd/torneios/Contest dos Comuna/A.cs
@@ -0,0 +1,19 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        /**
+	 * Extremamente Básico
+         * https://www.beecrowd.com.br/judge/pt/problems/view/1001 
+         */
+	
+	int a = Console.ReadLine();
+	int b = Console.ReadLine();
+	int x = a + b;
+	Console.WriteLine("X = " + x);
+
+    }
+
+}
---
+++ beecrowd/torneios/Contest dos Comuna/A/Program.cs
@@ -0,0 +1,9 @@
+﻿/**
+ * Extremamente Básico
+ * https://www.beecrowd.com.br/judge/pt/problems/view/1001 
+ */
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int x = a + b;
+Console.WriteLine("X = " + x);
