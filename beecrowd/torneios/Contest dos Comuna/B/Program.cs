804e07a1166b15ef7dd35a792fe3f4d0c2510420
Alan Tavares <alan1tavares@gmail.com>
2023-03-02 21:31:45 -0300

    resolução do Problema B do Torneio do Comunas

---
+++ beecrowd/torneios/Contest dos Comuna/B/Program.cs
@@ -0,0 +1,19 @@
+﻿/**
+ * Problema B - Conversão de Tempo
+ * https://www.beecrowd.com.br/judge/pt/problems/view/1019
+ */
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+int seg = x % 60;
+int min = x / 60;
+int h = 0;
+
+if ( min > 60 )
+{
+	h = min / 60;
+	min = min % 60;
+}
+
+Console.WriteLine($"{h}:{min}:{seg}");
+
