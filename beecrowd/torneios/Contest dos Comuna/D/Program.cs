c92b2cbc4622a244756277a6c394f79e7825da50
Alan Tavares <alan1tavares@gmail.com>
2023-03-05 16:42:09 -0300

    resolução do Problema D do Torneio do Comunas

---
+++ beecrowd/torneios/Contest dos Comuna/D/Program.cs
@@ -0,0 +1,13 @@
+﻿/**
+ * Problema D - Substituição em Vetor 
+ * https://www.beecrowd.com.br/judge/pt/problems/view/1172
+ * */
+
+int[] vec = new int[10];
+for (int i = 0; i < 10; i ++)
+{
+	int input = Convert.ToInt32(Console.ReadLine());
+	vec[i] = input > 0 ? input : 1;
+}
+
+for(int i = 0; i < vec.Length; i++) Console.WriteLine($"X[{i}] = {vec[i]}");
