342bf2e71879b66c8d774a4e2c86a11451351712
Alan Tavares <alan1tavares@gmail.com>
2023-03-05 16:10:57 -0300

    resolução do Problema C do Torneio do Comunas

---
+++ beecrowd/torneios/Contest dos Comuna/C/Program.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Problema C - Flores Florescem da França
+ * https://www.beecrowd.com.br/judge/pt/problems/view/1140 
+ * */
+
+while (true)
+{
+string input = Console.ReadLine();
+if (input == "*") return;
+string [] words = input.Split(' ');
+char firstLetter = input[0];
+
+string result = "Y";
+foreach(string word in words)
+{
+	if (!((word[0] == Char.ToLower(firstLetter)) || (word[0] == Char.ToUpper(firstLetter))))
+		
+	{
+		result = "N";
+		break;
+	}
+}
+Console.WriteLine(result);
+}
+
+
