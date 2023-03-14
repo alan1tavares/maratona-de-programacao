a5a292db2cdca08d5a528abb26d8b9b6152daf20
Alan Tavares <alan1tavares@gmail.com>
2023-03-14 08:14:55 -0300

    Execício A do 2o torneio

---
+++ beecrowd/torneios/Contest dos Comuna, Edição II/A/Program.cs
@@ -0,0 +1,12 @@
+﻿/**
+* Salário
+* https://www.beecrowd.com.br/judge/pt/problems/view/1008
+**/
+
+string funcionario = Console.ReadLine();
+double horasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+double salarioPorHora = Convert.ToDouble(Console.ReadLine());
+double salario = horasTrabalhadas * salarioPorHora;
+
+Console.WriteLine($"NUMBER = {funcionario}");
+Console.WriteLine("SALARY = U$ " + salario.ToString("0.00"));
