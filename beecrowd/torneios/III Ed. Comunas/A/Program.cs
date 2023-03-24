290af9bd4d407a3b9e1e2ce2e7da63afc2776424
Alan Tavares <alan1tavares@gmail.com>
2023-03-23 21:05:33 -0300

    Resolução do problema A

---
+++ beecrowd/torneios/III Ed. Comunas/A/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+/*
+* Problema A
+* 1069 - Diamantes e Areia
+**/
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string diamantes = Console.ReadLine();
+    Console.WriteLine(GetQuantidadeDiamantes(diamantes));
+}
+
+
+
+static int GetQuantidadeDiamantes(string diamantes)
+{
+    int quantidadeDiamantes = 0;
+    Stack<char> minerando = new Stack<char>();
+
+    for (int i = 0; i < diamantes.Length; i++)
+    {
+        if (diamantes[i] == '<' || diamantes[i] == '>')
+        {
+            if (minerando.Count > 0 && minerando.Peek() == '<' && diamantes[i] == '>')
+            {
+                minerando.Pop();
+                quantidadeDiamantes++;
+            }
+            else
+                minerando.Push(diamantes[i]);
+        }
+    }
+
+    return quantidadeDiamantes;
+}
+
+
+
+// Console.WriteLine(quantidadeDiamantes);
