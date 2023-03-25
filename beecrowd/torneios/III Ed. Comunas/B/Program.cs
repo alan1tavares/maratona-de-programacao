2ea8bbf15d07d3b3f9ebb97e500034a5c4396aef
Alan Tavares <alan1tavares@gmail.com>
2023-03-25 14:57:59 -0300

    Resolução do problema B

---
+++ beecrowd/torneios/III Ed. Comunas/B/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+* Problema B
+* Língua do P
+**/
+string input = Console.ReadLine()+'.';
+string output = "";
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] != 'p')
+        output += input[i];
+    else if (input[i+1] == 'p')
+    {
+        output += 'p';
+        i++;
+    }
+}
+
+Console.WriteLine(output.Remove(output.Length-1));
