74634cede716f1c7f86b8eb0623651792d731894
Alan Tavares <alan1tavares@gmail.com>
2023-03-25 15:02:50 -0300

    Outra solução para o problema B

--- beecrowd/torneios/III Ed. Comunas/B/Program.cs
+++ beecrowd/torneios/III Ed. Comunas/B/Program.cs
@@ -2,17 +2,9 @@
 * Problema B
 * Língua do P
 **/
-string input = Console.ReadLine()+'.';
-string output = "";
-for (int i = 0; i < input.Length; i++)
-{
-    if (input[i] != 'p')
-        output += input[i];
-    else if (input[i+1] == 'p')
-    {
-        output += 'p';
-        i++;
-    }
-}
 
-Console.WriteLine(output.Remove(output.Length-1));
+string input = Console.ReadLine();
+input = input.Replace("pp","&");
+input = input.Replace("p","");
+input = input.Replace("&","p");
+Console.WriteLine(input);
