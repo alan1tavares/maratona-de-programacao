01d2ddd1c3285f5fafd1ac282cc0f1d1e204187e
Alan Tavares <alan1tavares@gmail.com>
2023-03-25 14:20:04 -0300

    Feito o problema C

---
+++ beecrowd/torneios/III Ed. Comunas/C/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+* Problema C
+* 2455 - Gangorra
+**/
+
+string[] s = Console.ReadLine().Split(' ');
+int[] input = Array.ConvertAll(s, int.Parse);
+int fact =  (input[2] * input[3]) - (input[0] * input[1]);
+if (fact > 0) Console.WriteLine(1);
+else if (fact < 0) Console.WriteLine(-1);
+else Console.WriteLine(0);
