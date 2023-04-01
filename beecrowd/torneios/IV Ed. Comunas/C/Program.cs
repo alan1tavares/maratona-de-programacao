bf189f8900c155236d583a600cdfd03f3f04a4c2
Alan Tavares <alan1tavares@gmail.com>
2023-04-01 16:27:32 -0300

    solução 01 - estroura o tempo

---
+++ beecrowd/torneios/IV Ed. Comunas/C/Program.cs
@@ -0,0 +1,19 @@
+﻿/**
+ * Fila
+ * https://www.beecrowd.com.br/judge/pt/problems/view/2460
+ */
+using System.Collections.Generic;
+using System.Linq;
+
+Console.ReadLine();
+string[] input = Console.ReadLine().Split(' ');
+HashSet<string> fila = new HashSet<string>(input);
+Console.ReadLine();
+fila.ExceptWith(Console.ReadLine().Split(' '));
+string saida = "";
+foreach (string item in fila)
+{
+    saida += item + " ";
+}
+Console.WriteLine(saida.Remove(saida.Length-1));
+
