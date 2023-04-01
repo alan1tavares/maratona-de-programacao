0a6f926b006ac17715109d397c9d8f7b4bdf6ef7
Alan Tavares <alan1tavares@gmail.com>
2023-04-01 11:56:59 -0300

    Resolução do problema A do torneio

---
+++ beecrowd/torneios/IV Ed. Comunas/A/Program.cs
@@ -0,0 +1,23 @@
+﻿/**
+ * Array Hash
+ * https://www.beecrowd.com.br/judge/pt/problems/view/1257
+ */
+
+int asciiA = (int) 'A';
+
+int n =  Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    int valorHash = 0;
+    int l = Convert.ToInt32(Console.ReadLine());    
+    for (int j = 0; j < l; j++)
+    {
+        string input = Console.ReadLine();
+        for (int k = 0; k < input.Length; k++)
+        {
+            valorHash += ((int)input[k])-asciiA + j + k; 
+        }
+    }
+
+    Console.WriteLine(valorHash);
+}
