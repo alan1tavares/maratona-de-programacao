71f5ceb4f0ff11570e55bdf874f154c8b27d2149
Alan Tavares <alan1tavares@gmail.com>
2023-04-01 21:21:52 -0300

    Resolução do problema C do torneio

--- beecrowd/torneios/IV Ed. Comunas/C/Program.cs
+++ beecrowd/torneios/IV Ed. Comunas/C/Program.cs
@@ -2,36 +2,23 @@
  * Fila
  * https://www.beecrowd.com.br/judge/pt/problems/view/2460
  */
-using System.Collections.Generic;
+    using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
 
 var stopwatch = new Stopwatch();
 stopwatch.Start();
+
 Console.ReadLine();
 string[] input = Console.ReadLine().Split();
 Console.ReadLine();
 string[] filaSaida = Console.ReadLine().Split();
 
-Dictionary<string, int> elementosQueSairam = new Dictionary<string, int>(100000);
-for (int i = 0; i < filaSaida.Length; i++)
-{
-    elementosQueSairam.Add(filaSaida[i], 1);
-}
+HashSet<string> fila = new HashSet<string>(input);
 
-string saida = "";
-for (int i = 0; i < input.Length; i++)
-{
-    if(!elementosQueSairam.ContainsKey(input[i]))
-    {
-        saida += input[i];
-        if (i != input.Length - 1)
-            saida += " ";     
-    }
-}
+fila.ExceptWith(filaSaida);
 
-
-Console.WriteLine(saida);
+Console.WriteLine(string.Join(" ", fila));
 stopwatch.Stop();
 Console.WriteLine(stopwatch.Elapsed);
