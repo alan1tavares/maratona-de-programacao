1cae6112b9902a1b0191e56d94b26fd3a842a619
Alan Tavares <alan1tavares@gmail.com>
2023-04-11 23:21:33 -0300

    Soulção para o problema da questão A

--- beecrowd/torneios/V Ed. Comunas/A/Program.cs
+++ beecrowd/torneios/V Ed. Comunas/A/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+ * Carteiro
+ * https://www.beecrowd.com.br/judge/pt/problems/view/2448
+*/
+using System;
+using System.Collections.Generic;
+
+string quantidadeEntrada = Console.ReadLine();
+string[] inputCasas = Console.ReadLine().Split();
+string ondeOCarteiroEsta = inputCasas[0];
+Dictionary<string, int> casas = new Dictionary<string, int>();
+
+for (int i = 0; i < inputCasas.Length; i++)
+    casas.Add(inputCasas[i], i + 1);
+
+string[] mapaEntrega = Console.ReadLine().Split();
+int passsos = 0;
+
+for (int i = 0; i < mapaEntrega.Length; i++)
+{
+    passsos += Math.Abs(casas[mapaEntrega[i]] - casas[ondeOCarteiroEsta]);
+    ondeOCarteiroEsta = mapaEntrega[i];    
+}
+
+Console.WriteLine(passsos);
