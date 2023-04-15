65c05cca3ba16b946ee7d9002ec5c1fa6b6d8e4a
Alan Tavares <alan1tavares@gmail.com>
2023-04-15 17:32:33 -0300

    Solução do problema 2449 - Fechadura
    Problema C do torneio

---
+++ beecrowd/torneios/VI Ed. Comunas/C/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Fechadura
+ * https://www.beecrowd.com.br/judge/pt/problems/view/2449
+*/
+using System;
+
+int solucao = int.Parse(Console.ReadLine().Split()[1]);
+int[] pinos = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int quantidadeMovimentos = 0;
+
+for (int i = 0; i < pinos.Length; i++)
+{
+    if (pinos[i] == solucao)
+        continue;
+    if (pinos[i] > solucao)
+    {
+        int diferenca = pinos[i] - solucao;
+        pinos[i+1] -= diferenca;
+        quantidadeMovimentos += diferenca;
+    }
+    else
+    {
+        int diferenca = solucao - pinos[i];
+        pinos[i+1] += diferenca;
+        quantidadeMovimentos += diferenca;
+    }
+}
+Console.WriteLine(quantidadeMovimentos);
