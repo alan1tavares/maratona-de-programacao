cdafa17ba28fd5d9847596a4af1f87d54b004260
Alan Tavares <alan1tavares@gmail.com>
2023-04-22 10:55:37 -0300

    Melhorado o tempo da questão A do VI Ed.Comunas

--- beecrowd/torneios/VI Ed. Comunas/A/Program.cs
+++ beecrowd/torneios/VI Ed. Comunas/A/Program.cs
@@ -3,8 +3,10 @@
  * https://www.beecrowd.com.br/judge/pt/problems/view/1195
 */
 using System;
+using System.Text;
 
 int c = int.Parse(Console.ReadLine());
+StringBuilder result = new StringBuilder (500000);
 for (int i = 0; i < c; i++)
 {
     string n = Console.ReadLine();
@@ -14,9 +16,12 @@ for (int i = 0; i < c; i++)
         noRaiz.Add(item);
 
     Console.WriteLine($"Case {i+1}:");
-    Console.WriteLine($"Pre.: {noRaiz.GetPreOrdem().Trim()}");
-    Console.WriteLine($"In..: {noRaiz.GetInOrdem().Trim()}");
-    Console.WriteLine($"Post: {noRaiz.GetPosOrdem().Trim()}\n");
+    result.Clear();
+    Console.WriteLine($"Pre.:{noRaiz.GetPreOrdem(result)}");
+    result.Clear();
+    Console.WriteLine($"In..:{noRaiz.GetInOrdem(result)}");
+    result.Clear();
+    Console.WriteLine($"Post:{noRaiz.GetPosOrdem(result)}\n");
 }
 
 class No
@@ -47,37 +52,44 @@ class No
         }
     }
 
-    public string GetPreOrdem() => GetPreOrdem(this);
-
-    private string GetPreOrdem(No no)
-    {
-        if (no == null)
-            return "";
-        string result = " " + no.Valor;
-        result += GetPreOrdem(no.Esquerdo);
-        result += GetPreOrdem(no.Direito);
-        return result;
+    public string GetPreOrdem(StringBuilder result){
+        GetPreOrdem(this, result);
+        return result.ToString();
     }
 
-    public string GetInOrdem() => GetInOrdem(this);
-
-    private string GetInOrdem(No no)
+    private void GetPreOrdem(No no, StringBuilder result)
     {
-        if (no == null) return "";
-        string result = GetInOrdem(no.Esquerdo);
-        result += no.Valor + " ";
-        result += GetInOrdem(no.Direito);
-        return result;
+        if (no == null) return;
+        result.Append(" " + no.Valor);
+        GetPreOrdem(no.Esquerdo, result);
+        GetPreOrdem(no.Direito, result);
     }
 
-    public string GetPosOrdem() => GetPosOrdem(this);
-
-    private string GetPosOrdem(No no)
+    public string GetInOrdem(StringBuilder result)
     {
-        if (no == null) return "";
-        string result = GetPosOrdem(no.Esquerdo);
-        result += GetPosOrdem(no.Direito);
-        result += no.Valor + " ";
-        return result;
+        GetInOrdem(this, result);
+        return result.ToString();
+    }
+
+    private void GetInOrdem(No no, StringBuilder result)
+    {
+        if (no == null) return;
+        GetInOrdem(no.Esquerdo, result);
+        result.Append(" " + no.Valor);
+        GetInOrdem(no.Direito, result);
+    }
+
+    public string GetPosOrdem(StringBuilder result)
+    {
+        GetPosOrdem(this, result);
+        return result.ToString();
+    }
+
+    private void GetPosOrdem(No no, StringBuilder result)
+    {
+        if (no == null) return;
+        GetPosOrdem(no.Esquerdo, result);
+        GetPosOrdem(no.Direito, result);
+        result.Append(" " + no.Valor);
     }
 }
