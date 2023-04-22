71000879f27d61b2ca272826036b0984adcb72a5
Alan Tavares <alan1tavares@gmail.com>
2023-04-22 18:37:56 -0300

    Reolução do problema B da VI Ed. Comunas

---
+++ beecrowd/torneios/VI Ed. Comunas/B/Program.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * Gerando Permutações Ordenadas Rapidamente
+ * https://www.beecrowd.com.br/judge/pt/problems/view/1401
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+int.TryParse(Console.ReadLine(), out int quantidadeCasos);
+
+for (int i = 0; i < quantidadeCasos; i++)
+{
+    string input = Console.ReadLine();
+
+    HashSet<string> result = new HashSet<string>();
+    Permutacao(result, "", String.Concat(input.OrderBy(c => c)));
+
+    Console.WriteLine(string.Join("\n", result));
+    Console.WriteLine();
+}
+
+static void Permutacao(HashSet<string> permutacaoGerada, string permutacaoAtual,
+    string elementosAPermutar)
+{
+    if (elementosAPermutar != "")
+    {
+        for (int i = 0; i < elementosAPermutar.Length; i++)
+        {
+            string proximaPermutacao = permutacaoAtual + elementosAPermutar[i];
+            string elementosRestantes = elementosAPermutar.Remove(i, 1);
+            Permutacao(permutacaoGerada, proximaPermutacao, elementosRestantes);
+        }
+    } else
+        permutacaoGerada.Add(permutacaoAtual);
+}
