7a4a21514885b94bf390c0a32eaad3eefc53ad01
Alan Tavares <alan1tavares@gmail.com>
2023-04-22 22:17:11 -0300

    Organização do repositório

--- listas/guloso/C.py
+++
@@ -1,7 +0,0 @@
-testCase = int(input())
-
-for x in range(1, testCase + 1):
-    tamanhoCampo = input()
-    campo = input()
-    campo = campo.replace('.#', '1')
-    print("Case " + str(x) + ": " + str(campo.count('1')))
