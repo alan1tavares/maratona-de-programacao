35651be1bd758fe312a3e9eca44de5b90752ae59
Alan Tavares <alan1tavares@gmail.com>
2023-04-25 00:21:16 -0300

    in_progress: questão A torneio: VII Ed. Comunas
    Comecei uma implementação de uma lista linkada, mas tenho que
    alterar para uma lista circular. O c# não tem suporte para isso
    vou ter que implementar na mão, caso eu queira fazer dessa forma.

---
+++ beecrowd/torneios/VII Ed. Comunas/A/Program.cs
@@ -0,0 +1,48 @@
+﻿/**
+ * Acampamento de Férias
+ * https://www.beecrowd.com.br/judge/pt/problems/view/1167
+ */
+using System.Collections.Generic;
+
+while (true)
+{
+    int n = int.Parse(Console.ReadLine());
+    if (n == 0) break;
+    LinkedList<Pessoa> circulo = new LinkedList<Pessoa>();
+    for (int i = 0; i < n; i++)
+    {
+        string[] input = Console.ReadLine().Split();
+        circulo.AddLast(new Pessoa
+        {
+            Nome = input[0],
+            Ficha = int.Parse(input[1])
+        });
+    }
+
+    var no = circulo.First.Next;
+    int ficha = no.Value.Ficha;
+    int count = 1;
+    while(circulo.Count != 1)
+    {
+        if (count == ficha)
+        {
+            ficha = no.Value.Ficha;
+            var nextNo = ficha % 2 == 0 ? no.Previous : no.Next;
+            count = 1;
+            circulo.Remove(no);
+            no = nextNo;
+        }
+
+        no = (ficha % 2 == 0) ? no.Previous : no.Next;
+        count += 1;
+    }
+
+    Console.WriteLine("Vencendor(a): " + no.Value.Nome);
+
+}
+
+public record Pessoa
+{
+    public string Nome {get;set;}
+    public int Ficha {get;set;}
+}
